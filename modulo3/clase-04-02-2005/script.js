513c971c4008313615ac43c1b367873d0b877d84
nodracPM <nodraccpm14@gmail.com>
2025-02-04 21:44:26 -0600

    Agrega console.log para verificar todos los tipos de de variables

--- modulo3/clase-04-02-2005/script.js
+++ modulo3/clase-04-02-2005/script.js
@@ -32,3 +32,13 @@ var undefined
 */
 console.log(tittle, edad)
 console.log(typeof(largeNumber))
+
+console.log(typeof(42))
+console.log(typeof("Veinticinco"))
+console.log(typeof(-666))
+console.log(typeof(true))
+console.log(typeof(0))
+console.log(typeof(''))
+console.log(typeof(null))
+console.log(typeof(undefined))
+console.log(typeof(false))
