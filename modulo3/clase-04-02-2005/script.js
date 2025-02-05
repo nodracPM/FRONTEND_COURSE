b57c3a2fc5c064255838fdca9371f6399a752fb4
nodracPM <nodraccpm14@gmail.com>
2025-02-04 20:46:00 -0600

    Agrega instrucciones para verificar los tipos de variables

---
+++ modulo3/clase-04-02-2005/script.js
@@ -0,0 +1,34 @@
+//Tipo Number 
+var edad = 25
+var precio = 100.52
+var temperatura = -15
+
+//Tipo String
+var nombre = "Pepe"
+var tittle = "The story"
+
+//Tipo Boolean
+var isTrue = true 
+var isFalse = false 
+
+//Tipo Symbol
+var simbolo = Symbol("id")
+
+//Tipo BigInt
+var largeNumber = 1245677777777777777777777777n; 
+
+//Null, undefined y NaN 
+//Tipo Null (valor vacío intencionalmente)
+var vacio = null 
+
+//Tipo Undefined (Valor NO asignado)
+var undefined 
+
+//Tipo NaN
+
+
+/*console.log se usa para mostrar valores en la consola del navegador
+    Es útil para depurar y entender cómo funcionan las variables en el código 
+*/
+console.log(tittle, edad)
+console.log(typeof(largeNumber))
