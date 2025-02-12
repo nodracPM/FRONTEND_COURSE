7f93a548a356b20f86ee21e7ea78fbe21ed13828
nodracPM <nodraccpm14@gmail.com>
2025-02-11 20:32:38 -0600

    Add project february 6th

--- modulo3/clase-06-02-2025/script.js
+++ modulo3/clase-06-02-2025/script.js
@@ -19,6 +19,25 @@ Nota: siempre se evalua lo que esté entre paréntesis primero
 
 
 */
-let maria = "maria"
-console.log(0 == false)
-console.log(maria)
+
+function random(min,max) {
+    return Math.floor((Math.random())*(max-min+1))+min;
+}
+
+let note_number = random(0,100)
+let final_note
+
+if(note_number >=90) {
+    final_note = "Excelente"
+}
+else if(note_number >= 75 && note_number < 90) {
+    final_note = "Bien"
+}
+else if(note_number >= 60 && note_number < 75) {
+    final_note = "Suficiente"
+}
+else {
+    final_note = "Insuficiente"
+}
+
+console.log(final_note)
