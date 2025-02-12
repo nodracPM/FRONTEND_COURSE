c44fcf5837fc180a09fdf8368474197674245537
nodracPM <nodraccpm14@gmail.com>
2025-02-11 20:14:49 -0600

    Add project february 11th

---
+++ modulo3/clase-06-02-2025/script.js
@@ -0,0 +1,24 @@
+/*
+var: se puede reasignar y redeclarar en cualquier parte del código. 
+let: se puede reasignar pero no se puede redeclarar en el mismo ámbito (proyecto). 
+const: no se puede reasignar ni redeclarar 
+*/
+
+/* Operadores lógicos 
+
+
+Jerarquía de operadores lógicos
+1. NOT (!)
+2. AND (&&)
+3. OR (||)
+
+Nota: siempre se evalua lo que esté entre paréntesis primero 
+*/
+
+/* Estructuras de control if 
+
+
+*/
+let maria = "maria"
+console.log(0 == false)
+console.log(maria)
---
+++ modulo3/clase-11-02-2025/script.js
@@ -0,0 +1,5 @@
+frutas = [["apple", 3], ["banana", 1], ["cherry", 2], ["orange", 4], ["kiwi", 5]];
+
+for(let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i][0] + ": " + frutas[i][1])
+}
